b7bc30b0612031f590c5cd5af4c53904273339af
Lennert1 <56080764+Lennert1@users.noreply.github.com>
2022-07-13 11:25:07 +0200

    .

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,8 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+
 @Service
 public class QueryProcessor {
 
@@ -11,8 +13,20 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("which of the following numbers is the largest: 41, 538")) {
-            return "538";
+        } else if (query.contains("which of the following numbers is the largest: ")) {
+            String q = query.toString();
+            String[] spilt = q.split(": ");
+            spilt = spilt[1].split(", ");
+            int numbers = 0;
+
+            int max = 0;
+            for(int i = 0; i < spilt.length; i++) {
+                numbers = Integer.parseInt(spilt[i]);
+                max = Math.max(max, numbers);
+            }
+
+
+            return Integer.toString(max);
         } else if (query.contains("what is your name")) {
             return "Lennert";
         } else if (query.contains("name")) {
