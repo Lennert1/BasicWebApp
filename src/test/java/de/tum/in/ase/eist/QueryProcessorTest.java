aaf0d00fc9d3f053367f631f25c7d147ce2acec7
Lennert1 <56080764+Lennert1@users.noreply.github.com>
2022-07-13 11:04:13 +0200

    .

--- src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
+++ src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
@@ -16,8 +16,8 @@ class QueryProcessorTest {
 
 	@Test
 	void testKnowsAboutShakespeare() {
-		String actual = queryProcessor.process("Shakespeare");
-		if (!actual.contains("playwright")) {
+		String actual = queryProcessor.process("what is your name");
+		if (!actual.contains("Lennert")) {
 			fail("The QueryProcessor does not know about Shakespeare.");
 		}
 	}
